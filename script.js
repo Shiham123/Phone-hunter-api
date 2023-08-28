fa0150c3268b8eca23111bf8d1f6505699ae3178
Shiham123 <s36651786@gmail.com>
2023-08-28 13:17:54 +0600

    upload commit

---
+++ script.js
@@ -0,0 +1,68 @@
+const inputEl = document.getElementById('input');
+const itemEl = document.getElementById('item');
+const showMoreBtn = document.getElementById('show-more');
+
+let showMore = true;
+let dataSlice = 0;
+
+function getApi(searchText) {
+  fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+    .then((response) => response.json())
+    .then((data) => {
+      let dataSlice = data.data;
+      dataSlice = dataSlice.slice(0, 12);
+      itemEl.innerHTML = '';
+
+      dataSlice.forEach((item) => {
+        const { brand, phone_name, slug, image } = item;
+        const modifiedData = {
+          phoneName: brand,
+          phone: phone_name,
+          id: slug,
+          phoneImg: image,
+        };
+        displayItems(modifiedData);
+      });
+    });
+}
+
+function handleButton() {
+  const inputValue = inputEl.value;
+  getApi(inputValue);
+
+  showMore = false;
+  if (!showMore) {
+    showMoreBtn.style.display = 'block';
+  }
+}
+
+function displayItems(modifiedData) {
+  let sliceData = modifiedData;
+
+  const { phoneName, phone, id, phoneImg } = sliceData;
+
+  const createItemDiv = document.createElement('div');
+  createItemDiv.classList.add = `card w-96 bg-base-100 shadow-xl`;
+  createItemDiv.innerHTML = `
+            <figure>
+              <img
+                src="${phoneImg}"
+                alt="${phone}"
+              />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">${phoneName}</h2>
+              <p>${id}</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+  `;
+  itemEl.appendChild(createItemDiv);
+}
+
+showMoreBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+});
+
+getApi();
